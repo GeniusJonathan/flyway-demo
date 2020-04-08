0c19e0f1171d5ed578c493c8357789d8ede76457
Jonathan Li <jykhli@gmail.com>
2020-04-09 00:50:41 +0200

    flyway demo

---
+++ src/main/java/com/callicoder/flywaydemo/model/User.java
@@ -0,0 +1,37 @@
+package com.callicoder.flywaydemo.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    @Size(min = 1, max = 100)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    private String lastName;
+
+}
